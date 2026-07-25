794035506a4ed2636c11a5e3937365d7bbf4d052
raviprakashbadugu <raviprakashbadugu@users.noreply.github.com>
2026-07-25 15:27:32 +0000

    deploy: bf2cd29804fe91761e40877c71c8ca2f1af0a7b4

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'order-tracker-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.svg',
+  './api/orders.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
